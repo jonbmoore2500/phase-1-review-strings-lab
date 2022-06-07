4fc9867f07efec7aa013a848f6cbb1ee816ee7ac
Jonathan <jonathanmoorevla@gmail.com>
2022-06-07 17:04:10 -0500

    commit tree 2056610c5db98dda2db8f51a36c25db59c69f63e

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Jonathan Moore';
+
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+
+//let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = 'Welcome, ' + currentUser.slice(0, 1) + '!';
